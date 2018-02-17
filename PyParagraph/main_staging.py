9c38ecb61fd153b94cc8ea52eb03f77797594ffb
Risa <rprezzano@macfound.org>
2018-02-17 12:35:04 -0600

    Attempt to PyParagraph, not complete, but I tried!

---
+++ PyParagraph/main_staging.py
@@ -0,0 +1,36 @@
+# dependencies
+import os
+
+# storing file path
+file_one = 'Resources/paragraph_1.txt'
+file_two = 'Resources/paragraph_2.txt'
+
+# declaring & initializing variables
+letter_count = 0
+
+# opening file1 // maybe add a loop to account for file2
+with open(file_one, 'r') as txtfile:
+    # reading file
+    paragraph = txtfile.read()
+
+    # finding word count
+    word_count = paragraph.count(" ") + 1 # +1 to account for the last word
+
+    # finding sentence count
+    sentence_count = paragraph.count(".") + paragraph.count("!") + paragraph.count("?")
+
+    # finding average letter count
+    for character in paragraph:
+        if character.isalpha():
+            letter_count += 1 # counting how many characters are letters
+    avg_letter_count = letter_count/word_count
+
+    # finding average sentence length
+    avg_sentence = word_count/sentence_count
+
+# tests
+# print(word_count)
+# print(sentence_count)
+print(letter_count)
+# print(avg_letter_count)
+# print(avg_sentence)
